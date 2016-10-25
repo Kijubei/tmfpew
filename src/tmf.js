375368d2daab9854325c4a2ff4a51c45ed614231
kijubei <tolga.philipp.naziyok@uni-oldenburg.de>
2016-10-25 18:30:24 +0200

    Änderungen am Menu: * js baut jetzt die überschriften als ul und packt die unterpunkte als li rein * neue function die auf knopfdruck auf ul die unterpunkte runterscrollt (geht noch nicht!)
    neue probleme: evtl die ul nicht mehr als link definieren

--- src/tmf.js
+++ src/tmf.js
@@ -1,9 +1,36 @@
 $(document).ready(function () {
-	
-	function buildMenu() {
 
-		//Übersichtsliste in seite laden
-		$("#hidden-source").load("http://localhost/HTMLtransfer/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm #mw-content-text", function() {
+	// um errors auszugeben
+	window.onerror = function(msg, url, linenumber) {
+	    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
+	    return true;
+	}
+
+	//Übersichtsliste in seite laden (erst danach alles ausführen)
+	$("#hidden-source").load("http://localhost/HTMLtransfer/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm #mw-content-text", function() {
+
+		// das menu wird gebaut
+		buildMenu();
+
+
+		//Sachen von der jeweiligen Wiki Seite in  div laden
+		// BROKEN ?
+		$(".navigation-link").click(function() {
+			var url = $(this).attr("href");
+			//alert(url);
+			$("#wikicontainer").load(url + " #content");
+			//$(this).load(url + " .firstHeading");
+			return false;
+		});
+
+
+		// Die Überschriften Klappen weitere Items auf
+		$('.navigation-sublist').click(function() {
+			$('.navigation-item').slideDown();
+		});
+
+		function buildMenu() {
+
 			var h3Html = new Array;
 			var pHtml = new Array;
 			// Hänge für jedes Item ausser dem ersten ein li an die navi liste
@@ -12,51 +39,25 @@ $(document).ready(function () {
 					
 				} else {
 					// unterscheidung
-					if (this.tagName == "P") {
-						// ein neues li an die navi hängen, das die klasse item-sub hat und den inhalt von der quelle nimmt
-						$(".navigation-list").append($("<li></li>").addClass('navigation-list-item-sub').append($(this).html()) );
-						pHtml[i-1] = $(this).html();
+					if (this.tagName == "H3") {
 						
-					} else if (this.tagName == "H3") {
-						// ein neues li an die navi hängen, das die klasse item hat und den inhalt von der quelle nimmt (nur mw-headline)
-						$(".navigation-list").append($("<li></li>").addClass('navigation-list-item').append($(".mw-headline",$(this)).html()) );
+						// ein neues ul an die navi hängen, das die klasse sublist hat und den inhalt von der quelle nimmt (nur mw-headline)
+						$(".navigation-list").append($("<ul></ul>").addClass('navigation-sublist').append($(".mw-headline",$(this)).html()) );
 						h3Html[i-1] = $(this).html();
+						
+					} else if (this.tagName == "P") {
+						// ein neues li an die navi hängen, das die klasse item hat und den inhalt von der quelle nimmt
+						$(".navigation-sublist:last").append($("<li></li>").addClass('navigation-list-item').append($(this).html()) );
+						pHtml[i-1] = $(this).html();
 					}
 				}
-				i++;
 			});
 
-			//$(".navigation-list li").addClass("navigation-list-item"); // css an li
-
-			// für jedes navi li den richtigen link hinzufügen
-			/*$(".navigation-list-item").each( function(i,ele ){
-				$(ele).append(pHtml[i]);
-			});*/
-
-			$(".navigation-list-item").addClass("navigation-link floatingbutton"); // css an das neue li
+			$(".navigation-sublist").addClass("navigation-link floatingbutton"); // css an das neue li
 			$(".navigation-list-item-sub a").addClass("navigation-link floatingbutton"); // css an das neue li
-		});
-		
-	}
 
+		}
 
-
-
-	window.onerror = function(msg, url, linenumber) {
-	    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
-	    return true;
-	}
-
-	buildMenu();
-
-
-	//Sachen von der jeweiligen Wiki Seite in  div laden
-	$(".navigation-link").click(function() {
-		var url = $(this).attr("href");
-		//alert(url);
-		$("#wikicontainer").load(url + " #content");
-		//$(this).load(url + " .firstHeading");
-		return false;
 	});
 
 });
