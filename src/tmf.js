4568409b14d3fab9aec74be98db4c414f4f8c447
kijubei <tolga.philipp.naziyok@uni-oldenburg.de>
2016-11-16 17:07:42 +0100

    Save feature wurde hinzugefügt! (Achtung: das Laden feature ist noch nicht inklusive)
    * Das Textfeld merkt sich jetzt den eingegebenen Text von item zu Item
    bzw. leert es auch beim wechseln.
    
    * Im Hintergrund wird jetzt die variable saveSession geführt, die jeden
    eintrag in den Textboxen und jede favorisierung speichert.
    
    * der Save-Button überführt saveSession in eine JSON datei und bietet diese
    als download an.
    
    * Der Navi construktor algorithmus wurde verbessert und startet jetzt mit
    list-item1 (wenn nur ein p genutzt wird für die beschreibung der übersicht.)

--- src/tmf.js
+++ src/tmf.js
@@ -7,15 +7,15 @@ $(document).ready(function () {
 	}
 
 	// für development:
-	var wikiUrl = "http://localhost/tmfpew/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm";
+	//var wikiUrl = "http://localhost/tmfpew/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm";
 	//für live:
-	//var wikiUrl = "https://vf-mi.virt.uni-oldenburg.de/mediawiki/%C3%9Cbersicht";
+	var wikiUrl = "https://vf-mi.virt.uni-oldenburg.de/mediawiki/%C3%9Cbersicht";
 
 	// Speichert die aktuelle itemID
 	var currentItemId;
 
 	// Speichert welche Seiten favorisiert sind und was in dem Textfeldern steht
-	var saveSession = [];
+	var saveSession = {};
 
 	buildMenu(wikiUrl);
 
@@ -30,36 +30,32 @@ $(document).ready(function () {
 			saveSession["url"] = url;
 			// das menu wird gebaut
 			// Hänge für jedes Item ausser dem ersten ein li an die navi liste
+			var count = 0;
 			$("#mw-content-text").children().each(function(i, ele){
-				if (i == 0) {
+				// unterscheidung
+				if (this.tagName == "H3") {
 					
-				} else {
-					// unterscheidung
-					if (this.tagName == "H3") {
-						
-						// ein neues ul an die navi hängen, das die klasse sublist hat und den inhalt (in einem div) von der quelle nimmt (nur mw-headline)
-						$(".navigation-list").append( //hänge an die navi an ...
-							$("<ul></ul>").addClass('navigation-sublist').append( // eine ul mit der klasse sublist
-								$("<div></div>").addClass("floatingbutton").append( // hänge daran ein div der klasse floatingbutton
-									$(".mw-headline",$(this)).html()) // mit dem inhalt aus der quelle mw-headline
-							)
-						);
-						
-					} else if (this.tagName == "P") {
-						// ein neues li an die neuste sublist der navi hängen, das die klasse item hat und den inhalt von der quelle nimmt
-						$(".navigation-sublist:last").append( // an die letzte sublist
-							$("<li></li>").addClass('navigation-list-item').attr("id", "list-item"+i).append( // ein li anhängen mit der klasse list-item und einer ID
-								$(this).html())  // mit dem inhalt aus der quelle
-						);
-					}
+					// ein neues ul an die navi hängen, das die klasse sublist hat und den inhalt (in einem div) von der quelle nimmt (nur mw-headline)
+					$(".navigation-list").append( //hänge an die navi an ...
+						$("<ul></ul>").addClass('navigation-sublist').append( // eine ul mit der klasse sublist
+							$("<div></div>").addClass("floatingbutton").append( // hänge daran ein div der klasse floatingbutton
+								$(".mw-headline",$(this)).html()) // mit dem inhalt aus der quelle mw-headline
+						)
+					);
+					
+				} else if (this.tagName == "P") {
+					// ein neues li an die neuste sublist der navi hängen, das die klasse item hat und den inhalt von der quelle nimmt
+					$(".navigation-sublist:last").append( // an die letzte sublist
+						$("<li></li>").addClass('navigation-list-item').attr("id", "list-item"+count).append( // ein li anhängen mit der klasse list-item und einer ID
+							$(this).html())  // mit dem inhalt aus der quelle
+					);
+					count++;
 				}
 			});
 
 			//$(".navigation-sublist").addClass("floatingbutton"); // css an das neue li
 			$(".navigation-list-item a").addClass("navigation-link floatingbutton"); // css an das neue li
-
 			
-
 			// Die Überschriften Klappen weitere Items auf
 			$('.navigation-sublist').click(function() {
 				//$('.navigation-list-item').slideToggle("slow");
@@ -79,11 +75,7 @@ $(document).ready(function () {
 			$(".navigation-link").click(function() {
 				var itemUrl = $(this).attr("href"); // hole url von dem geklickten Item
 				
-				//wenn keine itemID gemerkt ignore
-				if (currentItemId != null) {
-					//aktuelles textfeld speichern
-					saveSession[currentItemId][1] = $("#textfeld textarea").val();
-				} 
+				saveText(); // textfeld speichern
 				
 				$("#wikicontainer").load(itemUrl + " #content"); //wiki reinladen
 				currentItemId = $(this).parent().attr("id"); //momentane itemID merken
@@ -100,10 +92,13 @@ $(document).ready(function () {
 		favorItem(currentItemId);
 	});
 
-	// save-button
+	// save-button: erzeugt eine JSON Datei mit texten im textfeld und welche items faviorisiert wurden
 	$("#save").click(function() {
-		var test = JSON.stringify(saveSession);
-		alert(test);
+		saveText(); // erstmal jetztigen text speichern
+		//speichert saveSession in JSON form und hängt noch ein prefix dran die für den download wichtig sind
+		var saved = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(saveSession));
+		$("#save").attr("href", "data:" + saved); // ändert die href des save buttons in den tatsächlichen inhalt den wir laden wollen (funzt echt)
+		$("#save").attr("download", "session.json"); // legt fest wie die datei heißen soll
 	});
 
 	// funktion bekommt eine item ID, sucht danach und gibt deren kind (also den link, bzw den button) einen orangen rand / oder macht ihn weg
@@ -116,6 +111,14 @@ $(document).ready(function () {
 				saveSession[itemId][0] = false;
 			}
 	        
+	}
+
+	function saveText() {
+		//wenn keine itemID gemerkt ignore
+		if (currentItemId != null) {
+			//aktuelles textfeld speichern
+			saveSession[currentItemId][1] = $("#textfeld textarea").val();
+		} 
 	}
 
 
