50cfd5e5d8fc3f67833200a7ced2f7a287e6138f
kijubei <tolga.philipp.naziyok@uni-oldenburg.de>
2016-11-23 17:35:16 +0100

    Load ist jetzt live!
    * Es wird eine von save erzeugte JSON datei eingelesen und man kann so die
    session fortsetzten.
    
    * Momentan wird das ganze von php in view.php per echo und ein paar sicherheitsmaßnahmen
    übergeben. Das muss evtl nochmal überarbeitet werden.
    
    * Einige neue funktionen und ein einges an umstrukturierung in tmf.js.

--- src/tmf.js
+++ src/tmf.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
 	    return true;
 	}
 
-	// bekommt die Url aus dem Storage von dem new button (siehe unten)
+	// bekommt die Url aus dem Storage von dem new button (siehe unten). Wichtig wenn es unterschiedliche Übersichtsseiten gibt.
 	var wikiUrl = sessionStorage.wikiUrl;
 	// für development:
-	//wikiUrl = "http://localhost/tmfpew/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm";
+	wikiUrl = "http://localhost/tmfpew/wikiseiten/%C3%9Cbersicht%20%E2%80%93%20pew%20TMF.htm";
 	
 	// Speichert die aktuelle itemID
 	var currentItemId;
@@ -17,7 +17,57 @@ $(document).ready(function () {
 	// Speichert welche Seiten favorisiert sind und was in dem Textfeldern steht
 	var saveSession = {};
 
+	// legt fest ob es sich um eine neue Session handelt oder nicht.
+	var neu = true; 
+
+	initializeSession();
+	
 	buildMenu(wikiUrl);
+
+	////// BUTTONS
+
+	// favorite-button
+	$("#favorite").click(function() {
+		favorItem(currentItemId);
+		return false; //damit keine normale link funktionalität unternommen wird
+	});
+
+	// save-button: erzeugt eine JSON Datei mit texten im textfeld und welche items faviorisiert wurden
+	$("#save").click(function() {
+		saveText(); // erstmal jetztigen text speichern
+		//speichert saveSession in JSON form und hängt noch ein prefix dran die für den download wichtig sind
+		var saved = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(saveSession));
+		$("#save").attr("href", "data:" + saved); // ändert die href des save buttons in den tatsächlichen inhalt den wir laden wollen (funzt echt)
+		$("#save").attr("download", "session.json"); // legt fest wie die datei heißen soll
+	});
+
+	// Der neu button schreibt einfach die übersichtsURL in die Storage Datei
+	// Würde man eine andere Übersicht für die Items nutzen wollen, muss nur der link geändert werden 
+	$("#neu").click(function() {
+		sessionStorage.wikiUrl = "https://vf-mi.virt.uni-oldenburg.de/mediawiki/%C3%9Cbersicht";
+	});
+
+	// Upload button (bisher nicht genutzt)
+	$('#upload').on('click', function() {
+		
+	});
+
+	//// FUNKTIONEN
+
+	// Es wird zum einen saveSession gefüllt und zum anderen die wikiUrl angepasst
+	function initializeSession() {
+		if (typeof loadedSession !== 'undefined') { // wenn loadedSession in view.php definiert wurde
+			try{ 
+				loadedSession = JSON.parse(loadedSession); // datei nach JSON überführen (bzw es versuchen)
+				restoreSession(loadedSession);
+				neu = false; // keine neue Session
+				wikiURL = loadedSession["url"]; // die url auf die geladene ändern
+			} catch (e) { //error
+				alert("Leider ist etwas mit ihrer Datei nicht in ordnung. Es wird stattdessen eine neue Session gestartet. Fehlerbeschreibung: " + e.message);
+				console.log(e.message);
+			}
+		}
+	}
 
 	// baut das Menu abhängig von der mitgegebenen wiki übersichtsseiten url
 	// WICHTIG: Weil viele klassen und ID's erst nach der Menuerstellung existieren, müssen viele funktionen hier rein,
@@ -26,8 +76,6 @@ $(document).ready(function () {
 
 		//Übersichtsliste in seite laden (erst danach alles ausführen)
 		$("#hidden-source").load(url+ " #mw-content-text", function() {
-			// Speichern welches Menu genutzt wird
-			saveSession["url"] = url;
 			// das menu wird gebaut
 			// Hänge für jedes Item ausser dem ersten ein li an die navi liste
 			var count = 0;
@@ -62,11 +110,20 @@ $(document).ready(function () {
 				$(this).children(".navigation-list-item").slideToggle("slow");
 			});
 
-			// baue die Variable um die Session zu speichern
-			$(".navigation-list-item").each(function(index, value) {
-				saveSession[$(value).attr("id")] = [false, ""]; // erstelle object mit allen ID's das jeweils speichern kann ob fav / text
-			});
+			// baue die Variable um die Session zu speichern (nur wenn neu)
+			if (neu) {
+				// Speichern welches Menu genutzt wird
+				saveSession["url"] = url;
 
+				$(".navigation-list-item").each(function(index, value) {
+					saveSession[$(value).attr("id")] = [false, ""]; // erstelle object mit allen ID's das jeweils speichern kann ob fav / text
+				});
+			} else { //sonst: alle favoriten einfügen (gehört eigentlich zu restoreSession)
+				Object.keys(saveSession).forEach(function(key,index) {
+						setFavorItem(key, saveSession[key][0]);
+				});
+
+			}
 
 			//textfeld leeren weil es bei F5 sonst noch gefüllt ist
 			$("#textfeld textarea").val("");
@@ -80,35 +137,28 @@ $(document).ready(function () {
 				$("#wikicontainer").load(itemUrl + " #content"); //wiki reinladen
 				currentItemId = $(this).parent().attr("id"); //momentane itemID merken
 				$("#textfeld textarea").val(saveSession[currentItemId][1]); // textfeld laden
-				return false;
+				return false; //damit keine normale link funktionalität unternommen wird
 			});
 
 		});
 
 	}
 
-	// favorite-button
-	$("#favorite").click(function() {
-		favorItem(currentItemId);
-		return false;
-	});
-
-	// save-button: erzeugt eine JSON Datei mit texten im textfeld und welche items faviorisiert wurden
-	$("#save").click(function() {
-		saveText(); // erstmal jetztigen text speichern
-		//speichert saveSession in JSON form und hängt noch ein prefix dran die für den download wichtig sind
-		var saved = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(saveSession));
-		$("#save").attr("href", "data:" + saved); // ändert die href des save buttons in den tatsächlichen inhalt den wir laden wollen (funzt echt)
-		$("#save").attr("download", "session.json"); // legt fest wie die datei heißen soll
-	});
-
-	$("#neu").click(function() {
-		sessionStorage.wikiUrl = "https://vf-mi.virt.uni-oldenburg.de/mediawiki/%C3%9Cbersicht";
-	});
-
 	// funktion bekommt eine item ID, sucht danach und gibt deren kind (also den link, bzw den button) einen orangen rand / oder macht ihn weg
 	function favorItem(itemId) {
 			if (saveSession[itemId][0] == false) { // nicht fav
+				$("#"+itemId).children().css("border-color", "orange"); // mach rand orange
+				saveSession[itemId][0] = true;
+			} else {
+				$("#"+itemId).children().css("border-color", "grey"); // mach wieder grau orange
+				saveSession[itemId][0] = false;
+			}
+	        
+	}
+
+	// funktion bekommt eine item ID und ein bool und setzt es dann je nach bool auf fav (true) oder nicht fav (false)
+	function setFavorItem(itemId, bool) {
+			if (bool) { // fav
 				$("#"+itemId).children().css("border-color", "orange"); // mach rand orange
 				saveSession[itemId][0] = true;
 			} else {
@@ -126,6 +176,28 @@ $(document).ready(function () {
 		} 
 	}
 
+	// Mit der funktion wird eine Session wiederhergestellt. Dies geschieht durch das füllen der saveSession Variable.
+	function restoreSession(loadedSession) {
 
+		// krasse neue weise um objecte zu iterieren
+		Object.keys(loadedSession).forEach(function(key,index) {
+		    // key: the name of the object key
+		    // index: the ordinal position of the key within the object 
+		    //console.log("saveSession: " + saveSession[key] + " | key: " + key + " | index: " + index + " | loadedSession[key]: " + loadedSession[key]);
+
+		    // saveSession füllen
+		    if (key == "url") {
+		    	saveSession["url"] = loadedSession["url"];
+		    } else {
+		    	saveSession[key] = loadedSession[key];
+		    	/*if (loadedSession[key][0] == true ) { // passiert jetzt an anderer stelle (weil zu dem Zeitpunkt das menu noch nicht da ist)
+		    		favorItem(key);
+		    	}*/ 
+		    }
+		    console.log("saveSession[0]: " + saveSession[key][0] + " | saveSession[1]: " + saveSession[key][1] + " | loadedSession[key]: " + loadedSession[key]);
+
+		});
+
+	}
 
 });
--- view.php
+++ view.php
@@ -12,12 +12,18 @@
 	<div id="main" >
 		
 	
-		<div>
-		<img id="tmf-logo" src="src/tmf-logo_klein.png">
-		<h1 id="headline">Patienteneinwilligungswiki</h1>
+		<div>.
+			<img id="tmf-logo" src="src/tmf-logo_klein.png">
+			<h1 id="headline">Patienteneinwilligungswiki</h1>
 		</div>
-
-		<?php readfile($_FILES["session"]['tmp_name']); ?><br>
+	 	
+	 	<script type="text/javascript">
+		// evtl unsicher, dies ist evtl nur eine vorläufige version
+		// hier wird die hochgeladene datei von php nach js gereicht. sonderzeichen werden so gut es geht abgefangen.
+		// Der Rest sollte dann in js abgefangen werden. Es wird getestet ob das reicht.
+	 	var loadedSession = <?php echo json_encode(file_get_contents($_FILES["session"]['tmp_name']), JSON_HEX_QUOT|JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS);?>;
+		</script>
+		
 
 		<div id="tmfcontent" class="clearfix">
 			<div id="wikicontainer">
